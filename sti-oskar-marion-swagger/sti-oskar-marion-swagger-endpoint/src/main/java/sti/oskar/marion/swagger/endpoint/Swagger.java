7b338afbcf729ef5850e6ef689e7066d66e0a820
Marion Michielsen <91435399+MarionMichielsen@users.noreply.github.com>
2022-02-01 14:04:03 +0100

    update swagger, daoimpl

---
+++ sti-oskar-marion-swagger/sti-oskar-marion-swagger-endpoint/src/main/java/sti/oskar/marion/swagger/endpoint/Swagger.java
@@ -0,0 +1,16 @@
+package sti.oskar.marion.swagger.endpoint;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+public class Swagger {
+
+    @SpringBootApplication
+    public class Swagger{
+
+        public static void main(String[] args) {
+            SpringApplication.run(Swagger.class, args);
+        }
+    }
+
+}
