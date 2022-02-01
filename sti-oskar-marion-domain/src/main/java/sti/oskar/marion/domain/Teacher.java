3983438f6f39d1937eeeca1cad769465ccbc13e7
Marion Michielsen <91435399+MarionMichielsen@users.noreply.github.com>
2022-02-01 11:11:54 +0100

    Changes by oskar

--- sti-oskar-marion-domain/src/main/java/sti/oskar/marion/domain/Teacher.java
+++ sti-oskar-marion-domain/src/main/java/sti/oskar/marion/domain/Teacher.java
@@ -1,6 +1,6 @@
 package sti.oskar.marion.domain;
-
 import java.util.*;
+import java.util.Objects;
 
 public class Teacher {
     private final String firstName;
@@ -25,4 +25,67 @@ public class Teacher {
         this.courses = builder.courses;
         this.salaryPerHour = builder.salaryPerHour;
 
+    }
+
+    public String getFirstName(){
+        return firstName;
+    }
+
+    public String getLastName(){
+        return lastName;
+    }
+
+    public int getId(){
+        return id;
+    }
+
+    public List<Course> getCourses(){
+        return courses;
+    }
+
+    public int getSalaryPerHour(){
+        return salaryPerHour;
+    }
+
+    @Override
+    public String toString(){
+        return firstName + " " + lastName;
+    }
+
+    public static Builder builder(){
+        return new Builder();
+    }
+
+    public static class Builder{
+        private String firstName;
+        private String lastName;
+        private int id;
+        private List<Course> courses;
+        private int salaryPerHour;
+
+        public Builder withFirstName(String firstName){
+            this.firstName = firstName;
+            return this;
+        }
+
+        public Builder withLastName(String lastName){
+            this.lastName = lastName;
+            return this;
+        }
+
+        public Builder withId(int id){
+            this.id = id;
+            return this;
+        }
+
+        public Builder withCourses(List<Course> courses){
+            this.courses = courses;
+            return this;
+        }
+
+        public Teacher build(){
+            return new Teacher(this.firstName, this.lastName, this.id, this.courses, this.salaryPerHour);
+        }
+
+    }
 }
